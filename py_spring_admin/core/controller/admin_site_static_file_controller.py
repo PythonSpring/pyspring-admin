abfcd4d5b52abe3cbbeffdaf9b7d9ad3f2add560
William Chen <william_w_chen@trendmicro.com>
2024-09-29 00:54:34 +0800

    add: Add additional doc-string for helping frontend dev building out project

--- py_spring_admin/core/controller/admin_site_static_file_controller.py
+++ py_spring_admin/core/controller/admin_site_static_file_controller.py
@@ -14,6 +14,14 @@ class AdminSiteStaticFileController(RestController):
 
     Admin can access the site via:
     http://0.0.0.0:8080/spring-admin/public/site
+    Frotend build base href should be set with /spring-admin/public/site:
+    For Angular, this can be done by running:
+        ng build --base-href /spring-admin/public/site/
+    For Vue:
+        Update vue.config.js: Create or update the vue.config.js file in the root of your Vue project:
+            module.exports = {
+                publicPath: '/spring-admin/public/site/'
+            };
     """
     
     DIST_DIR: ClassVar[str] = "py_spring_admin/core/controller/static/_dist"
