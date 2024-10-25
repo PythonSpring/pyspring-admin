e759b70ce1dd5535427ac51fc5051ff5ee6fc831
William Chen <william_w_chen@trendmicro.com>
2024-10-25 18:17:13 +0800

    add: register user should hash the password before saving to the app_user table

--- py_spring_admin/core/repository/user_service.py
+++ py_spring_admin/core/repository/user_service.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from py_spring_core import Component
 from pydantic import BaseModel
+from passlib.context import CryptContext
 
 from py_spring_admin.core.repository.models import User, UserRole
 from py_spring_admin.core.repository.user_repository import UserRepository
@@ -16,6 +17,7 @@ class RegisterUser(BaseModel):
 
 class UserService(Component):
     user_repo: UserRepository
+    password_context: CryptContext
 
     def find_user_by_user_name(self, user_name: str) -> Optional[User]:
         return self.user_repo.find_user_by_user_name(user_name)
@@ -25,11 +27,15 @@ class UserService(Component):
 
     def find_user_by_id(self, user_id: int) -> Optional[User]:
         return self.user_repo.find_by_id(user_id)
+    
+    def get_hashed_password(self, raw_password: str) -> str:
+        return self.password_context.hash(raw_password)
 
     def register_user(self, new_user: RegisterUser) -> User:
+        hashed_password = self.get_hashed_password(new_user.password)
         user = User(
             user_name=new_user.user_name,
-            password=new_user.password,
+            password= hashed_password, 
             email=new_user.email,
             role=new_user.role,
         )
